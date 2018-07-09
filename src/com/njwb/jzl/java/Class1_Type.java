8409fbef70095edb2eb193fcf8628e02889d4d69
ZaleJ <947703213@qq.com>
2018-07-09 14:19:11 +0800

    some types

--- src/com/njwb/jzl/java/Class1_Type.java
+++ src/com/njwb/jzl/java/Class1_Type.java
@@ -1,10 +1,17 @@
 package com.njwb.jzl.java;
 
+
 public class Class1_Type {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		char sex='男';
+		char s1 = '\\';
+		char s2 ='\"';
+		char s3 ='\'';
+		float f = 2.5f;
+		int i = (int)f;
+		System.out.println(i);
 	}
 
 }
