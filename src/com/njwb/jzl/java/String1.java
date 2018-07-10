e88180c34131c241adb4bbec7fbb4708def4d258
ZaleJ <947703213@qq.com>
2018-07-10 09:34:41 +0800

    String1

---
+++ src/com/njwb/jzl/java/String1.java
@@ -0,0 +1,18 @@
+package com.njwb.jzl.java;
+
+public class String1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		String string = ""+1;
+//		String string2 = 1+"";
+//		String string3 = "1";
+//		System.out.println(string.equals(string3));
+		
+		
+		String string = new String("123");
+		String string2 = new String("123");
+		System.out.println(string.equals(string2));
+	}
+
+}
