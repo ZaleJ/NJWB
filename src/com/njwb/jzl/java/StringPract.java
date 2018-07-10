130d4fec25636353cc2383cca5163a93bfdd7c30
ZaleJ <947703213@qq.com>
2018-07-10 17:05:58 +0800

    update control

--- src/com/njwb/jzl/java/StringPract.java
+++ src/com/njwb/jzl/java/StringPract.java
@@ -1,12 +1,24 @@
 package com.njwb.jzl.java;
 
+import java.util.Scanner;
+
 public class StringPract {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int score = 85;
-		char level = score > 90 ? 'A':(score>80?'B':(score>70?'C':(score>60?'D':'E')));
+		char level = score > 90 ? 'A' : (score > 80 ? 'B' : (score > 70 ? 'C' : (score > 60 ? 'D' : 'E')));
 		System.out.println(level);
+		Scanner scanner = new Scanner(System.in);
+		String string = "ss";
+		switch (string) {
+		default:
+			System.out.println("yeye");
+			break;
+		case "ss":
+			System.out.println("heiheihei");
+			break;
+		}
 	}
 
 }
